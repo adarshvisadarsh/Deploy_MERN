06d7672391f7701b7d1b75017597faefe337ddf0
Aadarsh Vishwakarma <aadarsh.v@finacussolutions.local>
2024-05-24 11:36:42 +0530

    Add all my files

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () => {
+  mongoose
+    .connect(
+      process.env.MONGO_URL,
+      { dbName: "MERN_STACK_EVENT_MESSAGE" }
+    )
+    .then(() => {
+      console.log("Connected to Database");
+    })
+    .catch((err) => {
+      console.log("Some error occured while connecting to database:", err);
+    });
+};
